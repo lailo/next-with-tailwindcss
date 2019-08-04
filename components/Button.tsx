cdbbf3953e4ba2729a7bc40292524a1e810294cf
Labinot Sadiki <sadiki@joshmartin.ch>
2019-08-04 22:40:59 +0200

    adds styling for default button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react'
 
 const Button: React.FunctionComponent = ({ children }) => {
-  return <button>{children}</button>
+  return (
+    <button className="bg-green-700 hover:bg-green-800 text-white px-4 py-2 ">
+      {children}
+    </button>
+  )
 }
 
 export default Button

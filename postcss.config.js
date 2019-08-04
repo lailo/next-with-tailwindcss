1887692a7ae32d4521cddc50bc516d6aa4b02997
Labinot Sadiki <sadiki@joshmartin.ch>
2019-08-04 21:55:25 +0200

    adds purgecss to postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelist: ["html", "body"]
+});
+
 module.exports = {
-  plugins: [require("postcss-import"), require("tailwindcss"), require("postcss-nested"), require("autoprefixer")]
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 };

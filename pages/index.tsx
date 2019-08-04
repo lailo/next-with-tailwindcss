5a26aeda620b92be1b627b2e9e191ded3762ac24
Labinot Sadiki <sadiki@joshmartin.ch>
2019-08-04 22:48:50 +0200

    adds secondary button to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,8 @@ const Page: NextPage<Props> = ({ userAgent }) => {
     <main className="text-center  h-screen bg-gray-200 py-16 px-8">
       <h1 className="text-4xl uppercase font-black mb-8">Your user agent</h1>
       <h2 className="m-w-50 mb-4">{userAgent}</h2>
-      <Button>Hello World</Button>
+      <Button>Primary (Default)</Button>
+      <Button type="secondary">Secondary</Button>
     </main>
   )
 }

5b94f7c7ab15b541c45ae47a48df2ab61393c4e1
Labinot Sadiki <sadiki@joshmartin.ch>
2019-08-04 22:35:41 +0200

    adds snapshot tests

--- components/Button.test.tsx
+++ components/Button.test.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 
 import Button from './Button'
 
 describe('Button', () => {
-  it('renders the checkbox with correct label', () => {
+  it('renders with correct label', () => {
     const wrapper = shallow(<Button>Hello</Button>)
     expect(wrapper.text()).toEqual('Hello')
+  })
+  it('matches snapshot', () => {
+    const wrapper = shallow(<Button>Snapshot</Button>)
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
-const TEST_REGEX = "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$";
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$'
 
 module.exports = {
-  setupFiles: ["<rootDir>/jest.setup.js"],
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testRegex: TEST_REGEX,
   transform: {
-    "^.+\\.tsx?$": "babel-jest"
+    '^.+\\.tsx?$': 'babel-jest',
   },
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  collectCoverage: false
-};
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverage: false,
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+}

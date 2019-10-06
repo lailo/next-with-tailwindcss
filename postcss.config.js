396041e8d13770121b7e5feb125a9c154fc81347
Labinot Sadiki <sadiki@joshmartin.ch>
2019-10-06 20:05:13 +0200

    adds postcss-preset-env package
    and removes the includes postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,14 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./components/**/*.tsx', './pages/**/*.tsx'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  whitelist: ["html", "body"]
-});
+  whitelist: ['html', 'body'],
+})
 
 module.exports = {
   plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("postcss-nested"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
-};
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-preset-env')({ stage: 1 }),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+}

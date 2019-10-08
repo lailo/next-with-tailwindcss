827be8d6627986220de1840cd95c1efe8bf53d77
Labinot Sadiki <sadiki@joshmartin.ch>
2019-10-08 20:29:32 +0200

    adds simple button to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { NextPage } from 'next'
 import Button from '../components/Button'
+import ButtonSimple from '../components/ButtonSimple'
 import '../styles/main.css'
 
 interface Props {
@@ -9,18 +10,40 @@ interface Props {
 
 const Page: NextPage<Props> = ({ userAgent }) => {
   return (
-    <main className="text-center  h-screen bg-gray-200 py-16 px-8">
-      <h1 className="text-4xl uppercase font-black mb-8">Your user agent</h1>
-      <h2 className="m-w-50 mb-4">{userAgent}</h2>
-      <Button className="mr-4">Primary (Default)</Button>
-      <Button className={{ 'mr-4': true }} type="secondary">
-        Secondary
-      </Button>
-      <Button className="mr-4" loading>
-        Create
-      </Button>
-      <Button disabled>Secondary</Button>
-    </main>
+    <div className="flex flex-col h-screen bg-gray-200 py-16 px-8">
+      <header>
+        <h1 className="text-center text-3xl font-black mb-4">
+          NextJS with TypeScript
+          <br />
+          and TailwindCSS
+        </h1>
+      </header>
+      <main className="flex-grow flex flex-col items-center my-10">
+        <div className="mt-5 p-4 shadow hover:shadow-xl inline-block">
+          <h2 className="m-w-50 mb-4">Button</h2>
+          <Button className="mr-4">Primary (Default)</Button>
+          <Button className={{ 'mr-4': true }} type="secondary">
+            Secondary
+          </Button>
+          <Button className="mr-4" loading>
+            Create
+          </Button>
+          <Button disabled>Secondary</Button>
+        </div>
+        <div className="mt-5 p-4 shadow hover:shadow-xl inline-block">
+          <h2 className="m-w-50 mb-4">Simple Button</h2>
+          <ButtonSimple className="mr-4">Primary (Default)</ButtonSimple>
+          <ButtonSimple className={{ 'mr-4': true }} type="secondary">
+            Secondary
+          </ButtonSimple>
+          <ButtonSimple className="mr-4" loading>
+            Create
+          </ButtonSimple>
+          <ButtonSimple disabled>Secondary</ButtonSimple>
+        </div>
+      </main>
+      <footer className="text-xs text-center text-gray-600">{userAgent}</footer>
+    </div>
   )
 }
 

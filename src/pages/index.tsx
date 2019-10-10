2ade193e580871ec2c0ba65c915d37a85d51c732
Labinot Sadiki <sadiki@joshmartin.ch>
2019-10-10 22:57:25 +0200

    adds demo container component to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { NextPage } from 'next'
 import Button from '../components/Button'
 import ButtonSimple from '../components/ButtonSimple'
+import DemoContainer from '../components/DemoContainer'
 import '../styles/main.css'
 
 interface Props {
@@ -10,39 +11,21 @@ interface Props {
 
 const Page: NextPage<Props> = ({ userAgent }) => {
   return (
-    <div className="flex flex-col h-screen bg-gray-200 py-16 px-8">
-      <header>
-        <h1 className="text-center text-3xl font-black mb-4">
-          NextJS with TypeScript
-          <br />
-          and TailwindCSS
-        </h1>
-      </header>
-      <main className="flex-grow flex flex-col items-center my-10">
-        <div className="mt-5 p-4 shadow hover:shadow-xl inline-block">
-          <h2 className="m-w-50 mb-4">Button</h2>
-          <Button className="mr-4">Primary (Default)</Button>
-          <Button className={{ 'mr-4': true }} type="secondary">
-            Secondary
-          </Button>
-          <Button className="mr-4" loading>
-            Create
-          </Button>
-          <Button disabled>Secondary</Button>
-        </div>
-        <div className="mt-5 p-4 shadow hover:shadow-xl inline-block">
-          <h2 className="m-w-50 mb-4">Simple Button</h2>
-          <ButtonSimple className="mr-4">Primary (Default)</ButtonSimple>
-          <ButtonSimple className={{ 'mr-4': true }} type="secondary">
-            Secondary
-          </ButtonSimple>
-          <ButtonSimple className="mr-4" loading>
-            Create
-          </ButtonSimple>
-          <ButtonSimple disabled>Secondary</ButtonSimple>
-        </div>
-      </main>
-      <footer className="text-xs text-center text-gray-600">{userAgent}</footer>
+    <div className="bg-gray-200">
+      <div className="h-screen container py-8 max-w-xl flex flex-col ">
+        <header>
+          <h1 className="text-center text-3xl font-black text-black mb-4">
+            NextJS with TypeScript and TailwindCSS
+          </h1>
+        </header>
+        <main className="flex-grow flex flex-col items-center my-10">
+          <DemoContainer Component={Button} title="Button" />
+          <DemoContainer Component={ButtonSimple} title="ButtonSimple" />
+        </main>
+        <footer className="text-xs text-center text-gray-600">
+          {userAgent}
+        </footer>
+      </div>
     </div>
   )
 }

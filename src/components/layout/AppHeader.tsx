2bc4dc4876dee72e42bd02b09f55054f91c633e1
Lailo <lailo@helio.exchange>
2020-06-03 20:47:38 +0200

    adds icons to header drop down menu

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -5,6 +5,8 @@ import dynamic from 'next/dynamic'
 import Dropdown from '@components/Dropdown'
 import routes from '@utils/routes'
 import { FiMoreHorizontal } from 'react-icons/fi'
+import { AiOutlineFontColors } from 'react-icons/ai'
+import { MdFeaturedPlayList } from 'react-icons/md'
 
 const ThemeSwitch = dynamic(() => import('@components/layout/ThemeSwitch'), {
   ssr: false,
@@ -34,23 +36,19 @@ const AppHeader: React.FC<Props> = ({ className }) => {
       </div>
       <div>
         <Dropdown buttonLabel={<FiMoreHorizontal />}>
-          <ul className="w-40 space-y-4 py-2">
+          <ul className="w-40 space-y-4 py-2 text-sm">
             <li>
               <Link {...routes.COMPONENTS}>
-                <a
-                  className="text-green-600 hover:underline"
-                  title="components"
-                >
+                <a className="text-primary hover:underline" title="components">
+                  <MdFeaturedPlayList className="inline mr-2" />
                   Components
                 </a>
               </Link>
             </li>
             <li>
               <Link {...routes.TYPOGRAPHY}>
-                <a
-                  className="text-green-600 hover:underline"
-                  title="Typography"
-                >
+                <a className="text-primary hover:underline" title="Typography">
+                  <AiOutlineFontColors className="inline mr-2" />
                   Typography
                 </a>
               </Link>
@@ -59,7 +57,7 @@ const AppHeader: React.FC<Props> = ({ className }) => {
               <hr className="dark:border-gray-700" />
             </li>
             <li className="flex items-center justify-between">
-              <span className="mr-auto text-sm text-gray-600">Theme</span>
+              <span className="mr-auto text-gray-600">Theme</span>
               <ThemeSwitch />
             </li>
           </ul>

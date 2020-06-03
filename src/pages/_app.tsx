398f56049d196a89cf1fea9afc810ca1ef031e8d
Lailo <lailo@helio.exchange>
2020-06-03 20:24:46 +0200

    adds router loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react'
+import Router from 'next/router'
 import { AppProps } from 'next/app'
+import NProgress from 'nprogress'
+
 import '@styles/main.css'
+import '@styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
